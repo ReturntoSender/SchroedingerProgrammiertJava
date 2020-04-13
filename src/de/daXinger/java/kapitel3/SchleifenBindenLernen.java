168bf73a4235e6740f99cc7252a850aeedae9dd4
daXinger <TerraXF@gmail.com>
2020-04-13 18:59:18 +0200

    Weiter in Kapitel 3

---
+++ src/de/daXinger/java/kapitel3/SchleifenBindenLernen.java
@@ -0,0 +1,9 @@
+package de.daXinger.java.kapitel3;
+
+public class SchleifenBindenLernen {
+	public static void main (String [] args) {
+		for (int i = 10; i > 0; i--) {
+			System.out.println(i);
+		}
+	}
+}
