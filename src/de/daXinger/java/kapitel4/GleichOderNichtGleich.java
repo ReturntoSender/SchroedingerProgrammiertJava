c6ed34aa14a4a06c7f4bb0b0830e4059d8adc9e2
terrax <terrax@192.168.0.112>
2020-05-13 20:43:00 +0200

    Kapitel 4 geht los

---
+++ src/de/daXinger/java/kapitel4/GleichOderNichtGleich.java
@@ -0,0 +1,9 @@
+package de.daXinger.java.kapitel4;
+
+public class GleichOderNichtGleich {
+	public static void main(String[] args) {
+		if(args[0] != null && args[0].contains("Schrödinger")) {
+			System.out.println("Hallo");
+		}
+	}
+}
